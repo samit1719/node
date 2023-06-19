d91e5557b775706d1adc5fe069dc051461b6d264
samit1719 <samuelteshome1719@gmail.com>
2023-06-19 11:40:42 +0300

    update

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-
-
-const fs = require('fs');
-
-
-
-
-
-
-// Deleting a file
-fs.unlink('example.txt', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('File deleted successfully.');
-});
+console.log("hi hi");
